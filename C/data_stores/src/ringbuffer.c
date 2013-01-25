7bc278fe97049d477f17c9f74f9d3c444401d9d9
Michael Schenck <michael@tumblr.com>
2013-01-25 09:29:50 -0500

    Corrected allocated/deallocation methods, and some minor cleanup

--- C/data_stores/src/ringbuffer.c
+++ C/data_stores/src/ringbuffer.c
@@ -13,49 +13,57 @@
 
 #define TESTMSG "127.0.0.1:48175 [21/Jan/2013:09:55:38.596] mainfrontend tumblelogpool/p-blogs-3a262277 0/0/0/5/5 403 1869 - - ---- 0/0/0/0/0 0/0 {collectd/5.0.3|} \"GET /tumblr-server-status?auto HTTP/1.1\""
 
+void
+name_region (char* region_str, int index)
+{
+  char        prefix_str[PREFIX_LEN];
+
+  strncpy(prefix_str, SERVERNAME, PREFIX_LEN);
+  sprintf(region_str, "/%s-%d", prefix_str, index);
+}
 
 /*
   # Generate a shm_region string
     - Used per ring buffer segment
     - Leverages SERVERNAME
-    - Format: 16 character string NAME-index
-      - NAME: created from SERVERNAME (only first 11 char)
+    - Format: REGION_LEN character string NAME-index
+      - NAME: created from SERVERNAME (only first PREFIX_LEN char)
       - index: from passed integer 
 */
-int
-init_rb (RingBuffer* rb, size_t segment_size, int segment_count)
+RingBuffer*
+init_rb (size_t segment_size, int segment_count)
 {
   RingBuffer  *current, *head, *last;
-  char        shm_region[16], shm_prefix[11];
   int         shm_fd, segment_iter;
+  char        shm_region[REGION_LEN];
 
   segment_iter = 0; 
   do {
-    // TODO: handle long SERVERNAME
-    strncpy(shm_prefix, "1234123412341234", 11);
-    sprintf(shm_region, "%s-%d", shm_prefix, segment_iter);
-    printf("The shm_region is '%s'\n", shm_region);
+    name_region(shm_region, segment_iter);
+    printf("The shm_region is '%s' and the size is %d bytes\n", shm_region, sizeof(RingBuffer));
 
-    shm_fd = shm_open(shm_region, O_RDWR|O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    shm_fd = shm_open(shm_region, O_RDWR|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+
     if (shm_fd == -1) {
       printf("FATAL: Unable to get file descriptor for segment %s\n", shm_region);
       return -1;
     } else {
-      printf("DEBUG:\tshm_region: %s\tshm_fd: %d\n", shm_region, (int)shm_fd);
+      printf("DEBUG:\tshm_region: %s\tfd: %d\n", shm_region, (int)shm_fd);
     }
 
-    if (ftruncate(shm_fd, sizeof(struct RingBuffer)) == -1) {
+    if (ftruncate(shm_fd, sizeof(RingBuffer)) == -1) {
       printf("FATAL: Unable to truncate shm segment %s\n", shm_region);
       return -1;
     }
 
-    current = mmap(NULL, sizeof(struct RingBuffer),
-      PROT_READ|PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    current = mmap(0, sizeof(RingBuffer), PROT_READ|PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
     if (current == MAP_FAILED) {
       printf("FATAL: Unable to map shm segment %s\n", shm_region);
       return -1;
     }
+
+    close(shm_fd); 
 
     if (segment_iter == 0) {
       head = current;
@@ -71,56 +79,39 @@ init_rb (RingBuffer* rb, size_t segment_size, int segment_count)
     last->next = head;
   }
 
-  return 0;
-}
-
-int
-free_rb (RingBuffer* rb, int segment_count)
-{
-  char        shm_region[16];
-  int         shm_fd, segment_iter;
-
-  segment_iter = 0; 
-  do {
-    sprintf(shm_region, "%s-%d", SERVERNAME, segment_iter);
-    segment_iter++;
-  } while ( segment_iter < segment_count );
- 
-  return 0;
+  return head;
 }
 
 void
-size_report (RingBuffer rb)
+free_rb (int segment_count)
 {
-  int pageSize = getpagesize();
-  printf ("Message size: %d bytes\n", BUFFERSIZE);
-  printf ("Messages per segment: %d\n", MSG_PER_SEGM);
-  printf ("Segment size: %d bytes\n", SEGSIZE);
-  printf ("Segment count: %li\n", SEGCOUNT);
-  printf ("Page size: %d\n", pageSize);
-  printf ("RingBuffer segment: %lu\n", sizeof(rb));
+  char        shm_region[REGION_LEN];
+  int         segment_index;
+
+  for (segment_index = 0; segment_index < segment_count; segment_index++) {
+    name_region(shm_region, segment_index);
+    shm_unlink(shm_region);
+  }
 }
 
-void
-write_log(RingBuffer* rb, char* entry)
-{
-  
-}
+
+
+
 
 int
 main (void)
 {
-  int           result, segments;
-  RingBuffer    rb;
+  int           seg_size, segments;
+  RingBuffer    *rb;
 
-  printf("Starting ... \n");
-  result = 0;
+  seg_size = 4096;
   segments = 12;
-  result = init_rb(&rb, 4096, segments);
 
-  //size_report(rb);
+  // Create buffer
+  rb = init_rb(seg_size, segments);
 
-  //result = free_rb(&rb, segments);
+  // Release buffer memory
+  free_rb(segments);
 
-  return result;
+  return 0;
 }
