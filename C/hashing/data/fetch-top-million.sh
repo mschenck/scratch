227f8e14389d0e189ebe61c455c1d829c208ade5
Michael Schenck <michael@tumblr.com>
2013-06-08 16:31:15 -0400

    A little more clean-up and some output for top million fetch script

--- C/hashing/data/fetch-top-million.sh
+++ C/hashing/data/fetch-top-million.sh
@@ -5,10 +5,15 @@ TOPMILLIONZIP="$DATADIR/top-million.zip"
 TOPMILLIONREPORT="$DATADIR/Quantcast-Top-Million.txt"
 TOPMILLIONFILE="$DATADIR/top-million.txt"
 
+echo "Pulling down Quantcast Top Million list"
 wget -O $TOPMILLIONZIP https://ak.quantcast.com/quantcast-top-million.zip
 
 if [[ -f $TOPMILLIONZIP ]]
 then
+  echo "Extracting the FQDNs"
   unzip -c $TOPMILLIONZIP > $TOPMILLIONREPORT
-  egrep '^[0-9]' $TOPMILLIONREPORT | awk '{print $2}' > $TOPMILLIONFILE
+  egrep '^[0-9]' $TOPMILLIONREPORT | awk '{print $2}' | sed -e 's///g' > $TOPMILLIONFILE
+else
+  echo "Failed to retrieve list"
+  exit 1
 fi
